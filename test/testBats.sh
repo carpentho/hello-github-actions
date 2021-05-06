aa79343201c9258d1a1d6524d8f9daba5bee545d
carpentho <64020041+carpentho@users.noreply.github.com>
2021-05-06 15:27:43 +0200

    Update testBats.sh

--- test/testBats.sh
+++ test/testBats.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bats
 @test "Should pass" {
-result="$(INPUT_MY_NAME='Mona' ./entrypoint.sh)"
+result="$(INPUT_MY_NAME='Mona' ./action-a/entrypoint.sh)"
 [ $? -eq 0 ]
 }
 @test "This test is a bug" {
-result="$(INPUT_MY_NAME='aMona' ./entrypoint.sh)"
+result="$(INPUT_MY_NAME='aMona' ./action-a/entrypoint.sh)"
 [ $? -eq 1 ]
 }
