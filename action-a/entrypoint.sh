b9a14cc6f17f5127e9776c02cc2c4a28d447ac8e
carpentho <64020041+carpentho@users.noreply.github.com>
2021-05-06 14:43:37 +0200

    Update entrypoint.sh.

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,9 @@
-#!/bin/sh -l
-
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+#!/usr/bin/env bats
+@test "Should pass" {
+result="$(INPUT_MY_NAME='Mona' ./entrypoint.sh)"
+[ $? -eq 0 ]
+}
+@test "This test is a bug" {
+result="$(INPUT_MY_NAME='aMona' ./entrypoint.sh)"
+[ $? -eq 1 ]
+}
