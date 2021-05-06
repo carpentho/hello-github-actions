6fb8890db1a53ca56afbe27d7895df367ffb590a
carpentho <64020041+carpentho@users.noreply.github.com>
2021-05-06 15:24:59 +0200

    Delete testBats.sh

--- testBats.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bats
-@test "Should pass" {
-result="$(INPUT_MY_NAME='Mona' ./entrypoint.sh)"
-[ $? -eq 0 ]
-}
-@test "This test is a bug" {
-result="$(INPUT_MY_NAME='aMona' ./entrypoint.sh)"
-[ $? -eq 1 ]
-}
